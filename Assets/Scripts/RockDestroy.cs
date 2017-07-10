674831d9002f69b2ada991db5088feb0fb2bcfce
Ben Cuan <ben.cuan@gmail.com>
2017-07-10 13:06:23 -0700

    Rock Frag Test

---
+++ Assets/Scripts/RockDestroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDestroy : MonoBehaviour {
+
+
+	public GameObject fragments;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col) {
+		Debug.Log("collide");
+		Instantiate(fragments, transform.position, transform.rotation);
+		Destroy(gameObject);
+	}
+}
